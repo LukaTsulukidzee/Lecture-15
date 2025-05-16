aa1bd52147d4e9ff468685b9dbf106d1549fcc5f
LukaTsulukidzee <tsulukidzeluka5@gmail.com>
2025-05-16 22:06:25 +0400

    core/structure
    adding html
    adding stylecss
    adding javascript

---
+++ script.js
@@ -0,0 +1,80 @@
+// let userName = "Luka";
+
+// console.log(userName[0]);
+// console.log(userName[0] + userName[0]);
+
+// let firstName = 10;
+// let b = "Your name is:";
+
+// console.log(b + userName);
+
+// let b = "hello";
+
+// alert("hello world");
+
+// confirm("Your age is under 10?");
+
+// let firstname = prompt("what is your name?");
+
+// console.log(firstname);
+
+let weather = "cold";
+
+// let a = 5;
+// let b = 10;
+// let c = 30;
+
+// if (a > b) {
+//   console.log("this is true");
+// } else if (a > c) {
+//   console.log("5-ი მეტია 30-ზე");
+// } else {
+//   console.log("ყველა სხვა დანარჩენი შედეგი");
+// }
+
+// let a = 0;
+// let b = 15;
+// let c = 40;
+
+// if (a < b) {
+//   console.log("this is positive");
+// } else if (a > c) {
+//   console.log("this is negative");
+// } else {
+//   console.log("ყველა სხვა შედეგი");
+// }
+
+// let firstColor = "witeli";
+// let secondColor = "yviteli";
+// let thirdColor = "mwvane";
+
+// let ourFriendAnswer = prompt("რა ფერია შუქნიშანი?");
+
+// if (ourFriendAnswer0 === firstColor) {
+//   console.log("გაჩერდი");
+// } else if (ourFriendAnswer === secondColor) {
+//   console.log("მოვემზადოთ");
+// } else if (ourFriendAnswer === thirdColor) {
+//   console.log("შეგიძლია წახვიდე");
+// } else {
+//   console.log("მე თვითონ ვცდილობ რომ შევამოწმო შუქნიშანი");
+// }
+
+let weatherOne = "raining";
+let weatherSecond = "cold";
+let weatherThird = "hot";
+let extraWeather = "snow";
+
+let themperature = "-10";
+
+OurfriendAnswer = prompt("როგორი ამინდია გარეთ?");
+
+if (OurfriendAnswer === weatherOne) {
+  console.log("wear a raincoat");
+} else if (weather === weatherSecond || weather === extraWeather) {
+  console.log("wear jacket");
+} else if (weather === weatherThird && themperature === "-10") {
+  console.log("wear t-shirt");
+} else {
+  console.log("wear normal clothes");
+}
